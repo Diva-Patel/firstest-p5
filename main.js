22a239cfe224c0ae97b9e38550a99fc88b912119
Diva-Patel <87813581+Diva-Patel@users.noreply.github.com>
2021-10-20 14:48:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function draw(){
+    image(video,0,0,300,300)
+    
+    }
+    
+    function take_snapshot(){
+       save('my_pic.jpeg')
+    }
+
+    function setup(){
+canvas=createCanvas(300,300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300)
+video.hide();
+    }
